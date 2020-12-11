cee4f4451acfcf1626b283c4a26e07d0c4147633
Elias Proctor <eliasproctor@Eliass-MacBook-Pro.local>
2020-12-11 14:42:58 -0600

    Finished transition and animation sections

--- box-model/main-project/shared.js
+++ box-model/main-project/shared.js
@@ -4,38 +4,46 @@ const modal = document.querySelector(".modal");
 const modalButton = document.querySelector(".modal__action--negative");
 const toggleButton = document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav");
+const ctaButton = document.querySelector(".main-nav__item--cta");
 
 const openModal = () => {
-    modal.classList.add('open');
-    backgrop.classList.add("open");
-}
+  modal.classList.add("open");
+  backgrop.classList.add("open");
+};
 
 const closeModal = () => {
-    if(modalButton)
-        modal.classList.remove('open');
-    backgrop.classList.remove("open");
-    closeMenu();
-}
+  if (modalButton) modal.classList.remove("open");
+  backgrop.classList.remove("open");
+  closeMenu();
+};
 
 const showMenu = () => {
-    mobileNav.classList.add('open');
-    backgrop.classList.add("open");
-}
+  mobileNav.classList.add("open");
+  backgrop.classList.add("open");
+};
 
 const closeMenu = () => {
-    mobileNav.classList.remove("open");
-}
+  mobileNav.classList.remove("open");
+};
 
-planButton.forEach(button=>{
-    button.addEventListener('click',openModal);
-})
+planButton.forEach((button) => {
+  button.addEventListener("click", openModal);
+});
 
-backgrop.addEventListener('click',closeModal);
+backgrop.addEventListener("click", closeModal);
 
-if(modalButton)
-    modalButton.addEventListener('click',closeModal);
+if (modalButton) modalButton.addEventListener("click", closeModal);
 
-toggleButton.addEventListener("click",showMenu);
+toggleButton.addEventListener("click", showMenu);
 
+ctaButton.addEventListener("animationstart", (e) => {
+  console.log("Animation started", e);
+});
 
+ctaButton.addEventListener("animationend", (e) => {
+  console.log("Animation ended", e);
+});
 
+ctaButton.addEventListener("animationiteration", (e) => {
+  console.log("Animation iteration", e);
+});
